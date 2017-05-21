12b8baeb33930d6d8c6693791cc5f4c20ba3a2b9
Keith Nelson <Keith.r.b.nelson@gmail.com>
2017-05-20 20:30:19 -0400

    polls app with generic Views tut 5

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,32 +3,45 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render, get_object_or_404
 
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, HttpResponseRedirect
 
-from .models import Question
+from .models import Question, Choice
 
 from django.template import loader
+
+from django.views import generic
+
+from django.urls import reverse
 
 # Create your views here.
 
 
-def index(request):
-  latest_question_list = Question.objects.order_by('-pub_date')[:5]
-  context = {
-      'latest_question_list': latest_question_list,
-  }
-  return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+  template_name = 'polls/index.html'
+  context_object_name = 'latest_question_list'
+
+  def get_queryset(self):
+    return Question.objects.order_by('-pub_date')[:5]
 
 
-def detail(request, question_id):
-  question = get_object_or_404(Question, pk=question_id)
-  return render(request, 'polls/detail.html', {'question': question})
+class DetailView(generic.DetailView):
+  """docstring for DetailView"""
+  model = Question
+  template_name = 'polls/detail.html'
 
 
-def results(request, question_id):
-  response = "you're looking at the results of question %s"
-  return HttpResponse(response % question_id)
+class ResultsView(generic.DetailView):
+  model = Question
+  template_name = 'polls/results.html'
 
 
 def vote(request, question_id):
-  return HttpResponse("you're voting on question %s" % question_id)
+  question = get_object_or_404(Question, pk=question_id)
+  try:
+    selected_choice = question.choice_set.get(pk=request.POST['choice'])
+  except (KeyError, Choice.DoesNotExist):
+    return render(request, 'polls/detail.html', {'question': question, 'error_message': "You didn't select a choice.", })
+  else:
+    selected_choice.votes += 1
+    selected_choice.save()
+    return HttpResponseRedirect(reverse('polls:results', args=(question_id)))
