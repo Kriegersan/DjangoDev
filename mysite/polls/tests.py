4a4fc71310a28c7e52749d69324f8f04ea080b79
Keith Nelson <Keith.r.b.nelson@gmail.com>
2017-05-23 12:49:19 -0400

    testing cont.

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -19,14 +19,33 @@ class QuestionMethodTests(TestCase):
     fquest = Question(pub_date=time)
     self.assertIs(fquest.was_published_recently(), False)
 
+  def test_was_published_recently_with_old_question(self):
+    time = timezone.now() - datetime.timedelta(days=30)
+    oquest = Question(pub_date=time)
+    self.assertIs(oquest.was_published_recently(), False)
 
-def test_was_published_recently_with_old_question(self):
-  time = timezone.now() - datetime.timedelta(days=30)
-  oquest = Question(pub_date=time)
-  self.assertIs(oquest.was_published_recently(), False)
+  def test_was_published_recently_with_recent_question(self):
+    time = timezone.now() - datetime.timedelta(hours=1)
+    rquest = Question(pub_date=time)
+    self.assertIs(rquest.was_publsihed_recently(), True)
 
 
-def test_was_published_recently_with_recent_question(self):
-  time = timezone.now() - datetime.timedelta(hours=1)
-  rquest = Question(pub_date=time)
-  self.assertIs(rquest.was_publsihed_recently(), True)
+def create_question(question_text, days):
+  time = timezone.now() + datetime.timedelta(days=days)
+  return Question.objects.create(question_text=question_text, pub_date=time)
+
+
+class QuestionViewTest(TestCase):
+
+  def test_index_view_withNoQuest(self):
+    response = self.client.get(reverse('polls:index'))
+    self.assertEqual(response.status_code, 200)
+    self.assertContains(response, "no polls are available.")
+    self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+  def test_index_view_withaPastQuest(self):
+    create_question(question_text="Past question.", days=-30)
+    response = self.client.get(reverse('polls:index'))
+    self.assertQuerysetEqual(response.context['latest_question_list'], ['<Question: Past question.>'])
+
+  def test_index_view_withaFutureQuest(self):
